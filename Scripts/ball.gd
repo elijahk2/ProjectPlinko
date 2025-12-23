006704f18b0ba749dfdc91bed8d0fcb3769d2494
Elliot Holmes <elliot.b.holmes@gmail.com>
2025-12-22 23:39:41 -0500

    added nicer (imo) ball collide sound, and made the collide sounds increase in pitch following a major scvale until 2 octaves

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -9,21 +9,28 @@ var dash_ready = 1
 const dash_power = 1200
 const tap_power = 2
 
+const init_pitch_scale = 0.8
+var scale_degree = 0
+
 func _ready():
 	position.x = randi_range(-200, 200)
-	hit_sound.pitch_scale = 0.9
+	hit_sound.pitch_scale = 1
 	self.set_collision_mask_value(1, true)
 	self.set_collision_mask_value(2, false)
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	if body.is_in_group("pegs"):
-		hit_sound.pitch_scale = hit_sound.pitch_scale + 0.1
 		var frame_count = animated_bg.sprite_frames.get_frame_count("BG Color Shift")
 		animated_bg.frame = (animated_bg.frame + 1) % frame_count
 		if animated_bg.frame == 0:
 			animated_bg.frame = 1
+		
+		# hit sound logic
 		hit_sound.play()
+		hit_sound.pitch_scale = init_pitch_scale * 2**(pitch_multiplier_power(scale_degree))
+		scale_degree += 1
+		
 		body.set_collision_layer_value(1, false)
 		body.set_collision_layer_value(2, true)
 		body.collision_mask = 0
@@ -46,3 +53,30 @@ func _physics_process(delta: float) -> void:
 		else:
 			apply_impulse(Vector2(tap_power,0),Vector2(0,0))
 		
+		
+# major scale
+func pitch_multiplier_power(degree: int) -> float:
+	# const scale = [1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 0.5]
+	var octaves = degree / 7
+	degree %= 7;  
+	var power_part = 0.0;
+	if degree == 0:
+		power_part = 0.0
+	if degree == 1:
+		power_part = 1.0/6.0
+	if degree == 2:
+		power_part = 2.0/6.0
+	if degree == 3:
+		power_part = 5.0/12.0
+	if degree == 4:
+		power_part = 7.0/12.0
+	if degree == 5:
+		power_part = 9.0/12.0
+	if degree == 6:
+		power_part = 11.0/12.0
+	var power = power_part + octaves
+	#stops at two octaves higher
+	if power > 2.0:
+		return 2.0
+	else:
+		return power
