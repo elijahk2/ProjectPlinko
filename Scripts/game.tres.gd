5790163aace95656229377a492d4ff9fe28853c4
elijahk2 <falknerjohnson88@gmail.com>
2025-12-23 12:15:15 -0500

    added more comments

--- Scripts/game.tres.gd
+++ Scripts/game.tres.gd
@@ -7,22 +7,22 @@ func _on_endzone_body_entered(body: RigidBody2D) -> void:
 #Procedural Peg Generation
 const NormalPeg = preload("res://Scenes/peg.tscn") #Load NormalPeg to spawn in
 
-var number_of_rows = 100
+var number_of_rows = 300
 var spawn_positions = [0,0,0,0,0,0,0,0,0,0]
-var spawn_chance = 10 # 1/spawn_chance = probability of spawning a peg on any given tile of a row
+var spawn_chance = 5 # 1/spawn_chance = probability of spawning a peg on any given tile of a row
 var row = 0 #Define iteration var
 var y_offset = 200 #Y distance between each row of 
 
 func create_peg_layout():
-	while row < number_of_rows:
-		row += 1
-		spawn_positions = [0,0,0,0,0,0,0,0,0,0]
+	while row < number_of_rows: #Repeat until all rows generated
+		row += 1 #increase iteration var
+		spawn_positions = [0,0,0,0,0,0,0,0,0,0] #Reset spawn map
 		for n in spawn_positions.size():
-			if randi_range(1,spawn_chance) == 1:
-				spawn_positions[n] = 1
-				var instance = NormalPeg.instantiate()
+			if randi_range(1,spawn_chance) == 1: #Randomly choose peg or empty
+				spawn_positions[n] = 1 #Fill spawn map with choice in location
+				var instance = NormalPeg.instantiate() #Create a new peg
 				instance.position = Vector2(75 * n - 340, y_offset * row) #340 = 375 - 1/2 Peg Width to fill row
-				self.add_child(instance)
+				self.add_child(instance) #Finish node creation
 		
 func _ready():
 	create_peg_layout()
