270878ef75ccf62d2dbdb252d90847f456d1cde6
elijahk2 <falknerjohnson88@gmail.com>
2026-01-18 16:52:54 -0500

    Added cursor move sfx

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -1,16 +1,25 @@
 extends Node
 
+#This node manages scene-to-scene interactions because it is set as a "global script"
+
 var sfx_player: AudioStreamPlayer
 var settings = []
 signal score_changed(new_score) # Define a signal to modify ScoreDisplay's score value
 
 func _ready():
 	sfx_player = AudioStreamPlayer.new()
-	sfx_player.bus = "Title SFX"
 	add_child(sfx_player)
 
 func play_title_start_sfx():
+	sfx_player.bus = "Title Play SFX"
+	sfx_player.pitch_scale = 1
 	sfx_player.stream = load("res://Assets/Sound/SFX/stringy.wav")
+	sfx_player.play()
+
+func play_cursor_move_sfx():
+	sfx_player.bus = "Title Cursor SFX"
+	sfx_player.pitch_scale = randf_range(0.8, 1.2)
+	sfx_player.stream = load("res://Assets/Sound/SFX/CursorMoveSound.wav")
 	sfx_player.play()
 	
 func bullet_peg_point_increment():
