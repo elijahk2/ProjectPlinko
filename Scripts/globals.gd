ded5f4172304b476e504d9eb4c8be9cb398bf81e
elijahk2 <falknerjohnson88@gmail.com>
2026-01-22 19:36:41 -0500

    Added title screen song logic

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -4,6 +4,8 @@ extends Node
 
 var sfx_player: AudioStreamPlayer
 var settings = []
+var song_notes = [1, 1.2, 1.4, 1.6, 1.8, 2, 1.8, 1.6, 1.4, 1.2] #Defines the song played when the cursor moves on the title screen
+var song_notes_id = 0
 signal score_changed(new_score) # Define a signal to modify ScoreDisplay's score value
 
 func _ready():
@@ -17,9 +19,10 @@ func play_title_start_sfx():
 	sfx_player.play()
 
 func play_cursor_move_sfx():
-	sfx_player.bus = "Title Cursor SFX"
-	sfx_player.pitch_scale = randf_range(0.8, 1.2)
-	sfx_player.stream = load("res://Assets/Sound/SFX/CursorMoveSound.wav")
+	sfx_player.bus = "Bounce SFX"
+	song_notes_id += 1
+	sfx_player.pitch_scale = song_notes[song_notes_id % song_notes.size()] - 0.4 #Select the proper note to play for the title scale
+	sfx_player.stream = load("res://Assets/Sound/SFX/BounceSound.wav")
 	sfx_player.play()
 	
 func bullet_peg_point_increment():
