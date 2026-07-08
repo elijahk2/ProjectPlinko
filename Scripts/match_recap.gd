dc5004aaf494bf9b0be5f339bda2bde100b421a7
elijahk2 <falknerjohnson88@gmail.com>
2026-07-08 19:37:08 -0400

    fixed match recaps

--- Scripts/match_recap.gd
+++ Scripts/match_recap.gd
@@ -1,16 +1,15 @@
 extends Control
 
-@onready var stats_label_1: Label = $StatsLabelsContainer/StatsLabel1
-@onready var stats_label_2: Label = $StatsLabelsContainer/StatsLabel2
-@onready var stats_label_3: Label = $StatsLabelsContainer/StatsLabel3
-@onready var stats_label_4: Label = $StatsLabelsContainer/StatsLabel4
-@onready var your_rank_is_label: Label = $StatsLabelsContainer/YourRankIsLabel
-@onready var ranking_label: Label = $StatsLabelsContainer/RankingLabel
-
-@onready var stats_labels_container: Control = $StatsLabelsContainer
+@onready var stats_label_1: Label = $StatsLabel1
+@onready var stats_label_2: Label = $StatsLabel2
+@onready var stats_label_3: Label = $StatsLabel3
+@onready var stats_label_4: Label = $StatsLabel4
+@onready var your_rank_is_label: Label = $YourRankIsLabel
+@onready var ranking_label: Label = $RankingLabel
+@onready var header: Label = $Header1
 
 var label_start_x = -300
-var label_vel = 200
+var label_vel = 500
 var is_intro_done = 0
 var i = 0
 var percentage_of_max = 0
@@ -29,13 +28,19 @@ func get_match_recap_from_globals():
 	max_score = Globals.max_score
 
 func _ready() -> void:
-	stats_labels_container.position.x = label_start_x
+	var curve = 25
+	stats_label_1.position.x = label_start_x
+	stats_label_2.position.x = label_start_x
+	stats_label_3.position.x = label_start_x
+	stats_label_4.position.x = label_start_x
+	your_rank_is_label.position.x = label_start_x
+	ranking_label.position.x = label_start_x
 	get_match_recap_from_globals()
 	stats_label_1.text = "Pegs Hit: " + str(pegs_hit)
 	stats_label_2.text = "Points Gained: " + str(points_gained)
 	stats_label_3.text = "Points Removed: " + str(points_removed)
 	stats_label_4.text = "Overall Score: " + str(points_gained - points_removed)
-	percentage_of_max = (float(points_gained - points_removed)) / max_score * 100
+	percentage_of_max = ((float(points_gained - points_removed)) / max_score * 100) + curve
 	if percentage_of_max <= 59: #Begin code to determine rank based on percentage
 		rank = "F"
 	elif percentage_of_max <= 62:
@@ -70,7 +75,28 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("push") and is_intro_done == 1:
 		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
 	if is_intro_done == 0:
-		stats_labels_container.position.x += label_vel * delta
-		i += 1
-		if i > 90:
+		if stats_label_1.position.x < 10:
+			stats_label_1.position.x += label_vel * delta / 1.5
+		else:
+			stats_label_1.position.x = 10
+		if stats_label_2.position.x < 10:
+			stats_label_2.position.x += label_vel * delta / 2
+		else:
+			stats_label_2.position.x = 10
+		if stats_label_3.position.x < 10:
+			stats_label_3.position.x += label_vel * delta / 2.5
+		else:
+			stats_label_3.position.x = 10
+		if stats_label_4.position.x < 10:
+			stats_label_4.position.x += label_vel * delta / 3
+		else:
+			stats_label_4.position.x = 10
 			is_intro_done = 1
+		if your_rank_is_label.position.x < 52:
+			your_rank_is_label.position.x += label_vel * delta / 2.5
+		else:
+			your_rank_is_label.position.x = 52
+		if ranking_label.position.x < 114:
+			ranking_label.position.x += label_vel * delta / 2
+		else:
+			ranking_label.position.x = 114
